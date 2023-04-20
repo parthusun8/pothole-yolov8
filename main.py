91802a2fac2a701484e977a4767e10fa86f3fe46
Parth Sundarka <77690791+parthusun8@users.noreply.github.com>
2023-04-20 22:07:57 +0530

    Added YOLO results

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from ultralytics import YOLO
 import cv2
-import cvzone
 import math
 
-cap = cv2.VideoCapture("../Videos/ppe-3.mp4")  # For Video
+cap = cv2.VideoCapture("videos/")  # For Video
 
-model = YOLO("ppe.pt")
+model = YOLO("pothole.pt")
 
 classNames = ['pothole']
 myColor = (0, 0, 255)
+
+
+def putTextRect(img, text, pos, scale=3, thickness=3, colorT=(255, 255, 255),colorR=(255, 0, 255),
+                offset=10, border=None, colorB=(0, 255, 0)):
+
+    ox, oy = pos
+    (w, h), _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_PLAIN, scale, thickness)
+
+    x1, y1, x2, y2 = ox - offset, oy + offset, ox + w + offset, oy - h - offset
+
+    cv2.rectangle(img, (x1, y1), (x2, y2), colorR, cv2.FILLED)
+    if border is not None:
+        cv2.rectangle(img, (x1, y1), (x2, y2), colorB, border)
+    cv2.putText(img, text, (ox, oy), cv2.FONT_HERSHEY_PLAIN, scale, colorT, thickness)
+
+
 while True:
     success, img = cap.read()
     results = model(img, stream=True)
@@ -18,9 +33,7 @@ while True:
             # Bounding Box
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            # cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,255),3)
             w, h = x2 - x1, y2 - y1
-            # cvzone.cornerRect(img, (x1, y1, w, h))
 
             # Confidence
             conf = math.ceil((box.conf[0] * 100)) / 100
@@ -32,7 +45,7 @@ while True:
                 if currentClass =='Potholes':
                     myColor = (0, 0,255)
 
-                cvzone.putTextRect(img, f'{classNames[cls]} {conf}',
+                putTextRect(img, f'{currentClass} {conf}',
                                    (max(0, x1), max(35, y1)), scale=1, thickness=1,colorB=myColor,
                                    colorT=(255,255,255),colorR=myColor, offset=5)
                 cv2.rectangle(img, (x1, y1), (x2, y2), myColor, 3)
